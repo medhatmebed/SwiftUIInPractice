b2963b540e00392ba881754d8dd95aa06e620252
medhatmeebed <Medhat@1pass.app>
2024-04-20 17:19:32 +0200

    Initial Commit

---
+++ SwiftUIInPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIInPractice
+//
+//  Created by Medhat Mebed on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIInPractice/SwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInPracticeApp.swift
+//  SwiftUIInPractice
+//
+//  Created by Medhat Mebed on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
